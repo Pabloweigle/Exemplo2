e03e60554fac3d55302aefa7a07df6ac965f7c32
Pabloweigle <47799975+Pabloweigle@users.noreply.github.com>
2019-02-26 21:50:16 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	system ("cls");
+	
+	int A=0;
+	
+	printf ("Informe um valor A inteiro: \n");
+	
+	scanf ("%i", &A);
+	
+	printf ("O valor de A eh: %i \n", A);
+	
+	system ("pause");
+	
+	
+	
+	
+	
+	return 0;
+}
